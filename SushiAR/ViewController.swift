dc0a4090af5b71afee7b0efb8118248f3f584ce1
Tulgaa <tulgaa.batbaatar@sjsu.edu>
2023-09-05 20:47:00 -0700

    Testing GIT

--- SushiAR/ViewController.swift
+++ SushiAR/ViewController.swift
@@ -16,9 +16,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        let boxAncchor = try! Experience.loadBox()
         
         // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        arView.scene.anchors.append(boxAncchor)
     }
 }
